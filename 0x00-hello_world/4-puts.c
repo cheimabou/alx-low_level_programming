0fc2ee0593e42651ee8a328f636df675d2a09a2a
cheimabou <cheimabou2585@gmail.com>
2023-08-12 14:46:04 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 /**
  * main - entry point
+ *
  * a program that print an expression using the puts funstion 
+ *
  * Return:always 0 (successful)
 */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");

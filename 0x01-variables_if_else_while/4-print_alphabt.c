f5ff60881efe94cf188cec6b2ab0941d4838bc71
cheimabou <cheimabou2585@gmail.com>
2023-08-13 13:44:12 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * a c program that prints all the alphabet except q and e using the putchar function
+ * Return: always 0 (succesfull)
+*/
+int main(void)
+{
+	char alph;
+
+	for (alph = 'a' ; alph <= 'z' ; alph++)
+	{
+		if (alph != 'q' || alph != 'e')
+			putchar(alph);
+	}
+	putchar('\n');
+	return (0);
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,3 +1,4 @@
 this program checks if a random number is positive, negative, or zero
 a c program that prints with putchar 
 this c program prints with putchar function lowercaese then uppercases letters
+a c program that prints all the alphabets except e and q

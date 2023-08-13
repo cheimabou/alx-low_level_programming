9b3bec5f1fbc7cce088f5b136e17282f67f2b3ea
cheimabou <cheimabou2585@gmail.com>
2023-08-13 12:59:30 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/*
+ * main - entry point
+ * a c program that prints with putchar function
+*/
+int main(void)
+{
+	char alph;
+
+	for (alph = 'a' ; alph <= 'z' ; alph++)
+		putchar(alph);
+	putchar('\n');
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,1 +1,2 @@
 this program checks if a random number is positive, negative, or zero
+a c program that prints with putchar 

fc707f13596576e334c996bfc7fdc2fbadd3b70e
cheimabou <cheimabou2585@gmail.com>
2023-08-13 13:24:01 -0700

    task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,9 +8,9 @@ int main(void)
 {
 	char low;
 	char upp;
-	for(low = 'a' ; low <= 'z' ; low++)
+	for (low = 'a' ; low <= 'z' ; low++)
 		putchar(low);
-	for(upp = 'A' ; upp <= 'Z' ; upp++)
+	for (upp = 'A' ; upp <= 'Z' ; upp++)
 		putchar(upp);
 	putchar('\n');
 	return (0);

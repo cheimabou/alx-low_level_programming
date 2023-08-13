517a319962949ad60adc05f82d45c4a17aa8013b
cheimabou <cheimabou2585@gmail.com>
2023-08-13 13:21:22 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * a c program that prints with putchar function lowercases then uppercases
+ * Return: always 0 (successful)
+*/
+int main(void)
+{
+	char low;
+	char upp;
+	for(low = 'a' ; low <= 'z' ; low++)
+		putchar(low);
+	for(upp = 'A' ; upp <= 'Z' ; upp++)
+		putchar(upp);
+	putchar('\n');
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,2 +1,3 @@
 this program checks if a random number is positive, negative, or zero
 a c program that prints with putchar 
+this c program prints with putchar function lowercaese then uppercases letters

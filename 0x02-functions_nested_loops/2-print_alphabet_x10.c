6492ba27ff802a7286fb5a238915f43dc38a1826
cheimabou <cheimabou2585@gmail.com>
2023-08-16 06:54:02 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - function prints alphabet x10 using _putchar function
+*/
+void print_alphabet_x10(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+			_putchar(j);
+	}
+	_putchar('\n');

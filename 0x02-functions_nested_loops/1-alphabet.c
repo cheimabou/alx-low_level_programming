91ac6813505022e8411089acd9f61470c373050c
cheimabou <cheimabou2585@gmail.com>
2023-08-16 06:29:41 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_alphabet: a function that prints alphabet using the _putchar function
+*/
+void print_alphabet(void)
+{
+	int i;
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+	_putchar(i);
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,1 +1,2 @@
  a c program that prints _putchar using my own header file
+a function that prints alphabet in lowercase using _putchar

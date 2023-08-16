650fe45cbe8fc6b33d66c460fbddc30163368184
cheimabou <cheimabou2585@gmail.com>
2023-08-16 06:34:17 -0700

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,12 @@
 #include "main.h"
 /**
- * print_alphabet: a function that prints alphabet using the _putchar function
+ * print_alphabet - a function that prints alphabet using the _putchar function
 */
 void print_alphabet(void)
 {
 	int i;
+
 	for (i = 'a'; i <= 'z'; i++)
 		_putchar(i);
-	_putchar(i);
+	_putchar('\n');
 }

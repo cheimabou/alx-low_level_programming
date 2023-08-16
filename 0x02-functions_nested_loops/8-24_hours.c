fa0bf31acb346f7aa8984d1ab2074a492f52f2fa
cheimabou <cheimabou2585@gmail.com>
2023-08-16 13:58:00 -0700

    task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * jack_bauer -  a function that prints from 00:00 to 23:59
+ * Return: always 0
+*/
+void jack_bauer(void)
+{
+	int i;
+	int j;
+	int k;
+	int m;
+
+	for (i = 0; i <= 2; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			for (k = 0; k <= 5; k++)
+			{
+				for (m = 0; m <= 9; m++)
+				{
+					_putchar('0' + i);
+					_putchar('0' + j);
+					_putchar(':');
+					_putchar('0' + k);
+					_putchar('0' + m);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+	return (0);
+}

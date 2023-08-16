8114a391c00609f64c8fd4d9936d042722134dda
cheimabou <cheimabou2585@gmail.com>
2023-08-16 11:37:28 -0700

    task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _isalpha - a function that checks a character if it is alphabet
+ * @c: parametr of the function
+ * Return: 1 if it is an alphabet otherwise 0
+*/
+int _isalpha(int c)
+{
+	if (c <= 122 && c >= 97 || c >= 65 && c <= 90)
+		return (1);
+	return (0);
+}

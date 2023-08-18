87d1f33e9190b3109768f60e3ae4f5e8da9d4803
cheimabou <cheimabou2585@gmail.com>
2023-08-18 13:34:14 -0700

    task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,33 +12,18 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0)
-		{
+		if (i % 3 == 0 && i % 5 != 0)
 			printf("Fizz");
-			if (i % 5 == 0)
-			{
-				printf("Buzz");
-				printf(" ");
-			}
-			else
-			{
-			printf(" ");
-			}
-		}
+		else if (i % 5 == 0 && i % 3 != 0)
+			printf("Buzz");
+		else if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
 		else
-		{
-			if (i % 5 == 0)
-			{
-				printf("Buzz");
-				printf(" ");
-			}
-			else
-			{
-				printf("%d", i);
-				printf(" ");
-			}
-		}
+			printf("%d", i);
+		if (i == 100)
+			printf("\n");
+		else
+			printf(" ");
 	}
-	printf("\n");
 	return (0);
 }

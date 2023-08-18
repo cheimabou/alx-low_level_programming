86fb1a2934ec8e17cd210ee4b17f9e9bdf8867e1
cheimabou <cheimabou2585@gmail.com>
2023-08-18 09:13:46 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_numbers - a function that prints from 0 to 9
+ * Return: the number
+*/
+void print_numbers(void)
+{
+	for (i == 48; i <= 57; i++)
+		_putchar(i);
+	_putchar('\n');
+}

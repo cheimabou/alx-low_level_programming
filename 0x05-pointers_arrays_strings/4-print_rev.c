4a0d40fa4f7681131b71e39c6607065c3e16b583
cheimabou <cheimabou2585@gmail.com>
2023-08-21 11:26:41 -0700

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_rev - a function that prints a
+ *                 reverse string
+ * @s: the variable that holds the @ of the string
+*/
+void print_rev(char *s)
+{
+	char *p = s;
+
+	while (*p == '\n')
+	{
+		p--;
+	}
+	while (p != s)
+	{
+		_putchar(*p);
+		p--;
+	}
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -1,3 +1,4 @@
  a function that swaps the values of two integers
  2- a function that returns the length of a string
  a function that prints a string 
+a function that prints a string in reverse

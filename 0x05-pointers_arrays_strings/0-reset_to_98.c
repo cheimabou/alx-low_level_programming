2a8f6dff2c29f5f79f7edd239ecc2a45599ef1ee
cheimabou <cheimabou2585@gmail.com>
2023-08-21 07:01:19 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main_h"
+/**
+ * reset_to_98 - a function that reset the value of the parameter to 98
+ * @n: the parameter
+*/
+void reset_to_98(int *n)
+{
+	int var;
+
+	var = 98;
+	n = &var;
+}

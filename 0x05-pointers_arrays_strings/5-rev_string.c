98b714cd06f45c141a3927a5e7db523202578aa8
cheimabou <cheimabou2585@gmail.com>
2023-08-22 13:03:59 -0700

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * rev_string - a function that reverse a string
+ * @s: the pointer that points the string
+*/
+void rev_string(char *s)
+{
+	int i, j, c;
+	char *k = s;
+	char p;
+
+	c = 0;
+	while (*k != '\0')
+	{
+		k++;
+		c++;
+	}
+	for (i = 0; i < c / 2; i++)
+	{
+		p = s[i];
+		s[i] = s[c - i - 1];
+		s[c - i - 1] = p;
+	}
+}

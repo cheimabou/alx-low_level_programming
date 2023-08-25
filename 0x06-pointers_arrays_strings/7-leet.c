dfc2732f190758ee66d9fe70103ccd3fb04cf745
cheimabou <cheimabou2585@gmail.com>
2023-08-25 10:33:09 -0700

    task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,5 +1,24 @@
 #include "main"
-/***/
-char *leet(char *)
+/**
+ * *leet - a function that encodes a string into 1337
+ * @a: the string
+ * Return: the result
+*/
+char *leet(char *a)
 {
+	char *p = a;
+	char arr[] = {'A', 'E', 'O', 'T', 'L'};
+	int val[] = {4, 3, 0, 7, 1};
+	int i;
 
+	while (*a != '\0')
+	{
+		for (i = 0; i < sizeof(arr) / sizeof(char); i++)
+		{
+			if (*a == arr[i] || *a == arr[i] + 32)
+				*a = 48 + val[i];
+		}
+		a++;
+	}
+	return (p);
+}

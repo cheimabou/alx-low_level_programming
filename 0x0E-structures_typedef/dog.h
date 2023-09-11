b967d79d436fbcb14cc168f4eec01cf0ee1aba17
cheimabou <cheimabou2585@gmail.com>
2023-09-11 12:10:58 -0700

    task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #ifndef new_type
 #define new_type
-
+/**
+ * struct dog - a dog's basic informations
+ * @name: name of the dog
+ * @age: its age
+ * @owner: its owner's name
+ * Description: Long desc
+*/
 struct dog
 {
 	char *name;

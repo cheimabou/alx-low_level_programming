3df3abc769d6335b1c3f815355c5e72fcadb4249
cheimabou <cheimabou2585@gmail.com>
2023-09-15 09:01:06 -0700

    task 0

--- 0x10-variadic_functions/main.h
+++ 0x10-variadic_functions/main.h
@@ -1,6 +1,5 @@
 #ifndef main_h
 #define main_h
-#include <stdio.h>
 #include <stdarg.h>
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);

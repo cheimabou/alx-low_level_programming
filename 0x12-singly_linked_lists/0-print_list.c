b9fe82392a340bc408e2f131aa48a69727087bb2
cheimabou <cheimabou2585@gmail.com>
2023-09-21 12:10:41 -0700

    task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * print_list - function that prints all the elements of a list
+ * @h: pointer to the list
+ * Return: number of elements
+*/
+size_t print_list(const list_t *h)
+{
+	const list_t *p = h;
+	size_t s = 0;
+
+	while (p)
+	{
+		if (p->str == NULL)
+			printf("[0] (nil)");
+		else
+			printf("[%d] %s\n", p->len, p->str);
+		p = p->next;
+		s++;
+	}
+	return (s);
+}

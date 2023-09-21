288b918971ae68aa6d0f7bb7a96653f42e1f4a9a
cheimabou <cheimabou2585@gmail.com>
2023-09-21 12:28:14 -0700

    task 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * list_len - function that calculates the number of elements in a linked list
+ * @h: pointer to the list
+ * Return: the result
+*/
+size_t list_len(const list_t *h)
+{
+	size_t s;
+
+	s = 0;
+	while (h != NULL)
+	{
+		s++;
+		h = h->next;
+	}
+	return (s);
+}

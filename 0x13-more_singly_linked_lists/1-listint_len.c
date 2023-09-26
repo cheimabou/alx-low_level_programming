ed7a1f9811cb9ce63788efbfe8a29e2bc8fa9637
cheimabou <cheimabou2585@gmail.com>
2023-09-26 12:22:13 -0700

    task 1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * listint_len - a function that returns the length of a list
+ * @h: pointer to the list
+ * Return: the result
+*/
+size_t listint_len(const listint_t *h)
+{
+	size_t s;
+
+	while (h != NULL)
+	{
+		s++;
+		h = h->next;
+	}
+	return (s);
+}
